547ae3a312aff1b9ceafbc173b734f1141793df3
takeshi <goozooloo123@gmail.com>
2024-09-23 17:43:49 +0900

    profile モデルの作成, db:migrate 実行, routes / controller / view 作成

---
+++ app/models/profile.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: profiles
+#
+#  id           :bigint           not null, primary key
+#  birthday     :date
+#  gender       :integer
+#  introduction :text
+#  subscribed   :boolean          default(FALSE)
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  user_id      :bigint           not null
+#
+# Indexes
+#
+#  index_profiles_on_user_id  (user_id)
+#
+class Profile < ApplicationRecord
+
+    def avatar_image
+        'default-avatar.png'
+        # if profile&.avatar&.attached?
+        #   profile.avatar
+        # else
+        #   'default-avatar.png'
+        # end
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_09_22_221218) do
+ActiveRecord::Schema.define(version: 2024_09_23_082246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.text "introduction"
+    t.integer "gender"
+    t.date "birthday"
+    t.boolean "subscribed", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
