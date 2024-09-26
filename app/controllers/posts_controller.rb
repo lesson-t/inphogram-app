1e526398939c3060562c9ce0852b3bc12970f46b
takeshi <goozooloo123@gmail.com>
2024-09-26 12:14:37 +0900

    画像複数を投稿できるように修正, 投稿者情報 / 投稿時間 / 投稿内容の表示, シェアボタンの配置

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -44,7 +44,7 @@ class PostsController < ApplicationController
 
     private
     def post_params
-        params.require(:post).permit(:content)
+        params.require(:post).permit(:content, photos: [])
     end
 
     def set_post
--- app/models/post.rb
+++ app/models/post.rb
@@ -14,4 +14,9 @@
 #
 class Post < ApplicationRecord
     belongs_to :user
+    has_many_attached :photos
+
+    def display_created_at
+        I18n.l(self.created_at, format: :default)
+    end
 end
