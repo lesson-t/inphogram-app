c9441934b489620ffeae037d3803bdf450efa1d1
takeshi <goozooloo123@gmail.com>
2024-09-27 12:08:04 +0900

    routes 設定, controller index / new / create 作成, view index / new 作成

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,25 @@
+class CommentsController < ApplicationController
+    def index
+        post = Post.find(params[:post_id])
+        @comments = post.comments
+    end
+
+    def new
+        post = Post.find(params[:post_id])
+        @comment = post.comments.build()
+    end
+
+    def create
+        post = Post.find(params[:post_id])
+        @comment = post.comments.build(comment_params)
+        @comment.save!
+        redirect_to post_comments_path(@comment), notice: 'Saved successfully'
+        # render json: @comment
+    end
+
+    private
+    def comment_params
+        params.require(:comment).permit(:content)
+    
+    end
+end
