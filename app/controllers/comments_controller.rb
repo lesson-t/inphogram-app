52a7e891517bfeac5bdbc721cfaa1a95fa55f17a
takeshi <goozooloo123@gmail.com>
2024-09-27 15:12:21 +0900

    Ajax によるコメントの投稿機能の実装, 要改良：コメント部分のAjaxによる反映

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,21 @@
 class CommentsController < ApplicationController
     def index
-        post = Post.find(params[:post_id])
-        @comments = post.comments
+        @post = Post.find(params[:post_id])
+        @comments = @post.comments
     end
 
-    def new
-        post = Post.find(params[:post_id])
-        @comment = post.comments.build()
-    end
+    # def new
+    #     @post = Post.find(params[:post_id])
+    #     @comments = @post.comments
+    #     render json: @comments
+    # end
 
     def create
         post = Post.find(params[:post_id])
         @comment = post.comments.build(comment_params)
         @comment.save!
-        redirect_to post_comments_path(@comment), notice: 'Saved successfully'
-        # render json: @comment
+        # redirect_to post_comments_path(@comment), notice: 'Saved successfully'
+        render json: @comment
     end
 
     private
