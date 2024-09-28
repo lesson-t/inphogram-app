bb5961c2b5a8783b9dcaf8032d738f354007bb1e
takeshi <goozooloo123@gmail.com>
2024-09-29 07:57:35 +0900

    フォロー機能の実装, フォロワーとの関係付け実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,12 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
   has_one :profile, dependent: :destroy
 
   def has_written?(post)
@@ -35,6 +41,10 @@ class User < ApplicationRecord
 
   def has_liked?(post)
     likes.exists?(post_id: post.id)
+  end
+
+  def follow!(user)
+    following_relationships.create!(following_id: user.id)
   end
 
   def avatar_image
