31b17c27c7107c2b8c01a19850de240cae00c417
takeshi <goozooloo123@gmail.com>
2024-10-02 13:58:06 +0900

    テストを context に書き換え

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,18 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it 'コンテンツと画像ファイルが入力されていれば、記事を保存できる' do
-    user = User.create!({
-      email: 'tester@example.com',
-      password: 'password',
-      name: 'tester'
-    })
-    post = user.posts.build({
-      content: Faker::Lorem.characters(number: 10),
-      photos: [fixture_file_upload(Rails.root.join('spec', 'fixtures', 'files', 'test1.png'), 'image/png')]
-    })
+  context 'コンテンツと画像ファイルが入力されている場合' do
+    before do
+      user = User.create!({
+        email: 'tester@example.com', 
+        password: 'password', 
+        name: 'tester'
+      })
+      @post = user.posts.build({
+        content: Faker::Lorem.characters(number: 10),
+        photos: [fixture_file_upload(Rails.root.join('spec', 'fixtures', 'files', 'test1.png'), 'image/png')]
+      })
+    end
 
-    expect(post.photos).to be_attached
-    expect(post).to be_valid
+    it '記事を保存できる' do
+      expect(@post.photos).to be_attached
+      expect(@post).to be_valid
+    end
   end
 end
