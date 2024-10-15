9b876beb746d2cf2d016d4d7373f59b7bc9ed3ef
takeshi <goozooloo123@gmail.com>
2024-10-16 08:28:41 +0900

    comment.js 画面遷移によるエラー画面確認・修正, 一度の投稿で複数回投稿されることを確認・修正

--- app/javascript/packs/comment.js
+++ app/javascript/packs/comment.js
@@ -1,52 +1,28 @@
 import $ from 'jquery'
 import axios from 'modules/axios'
 
-// const handleCommentForm = () => {
-//     $('.show-comment-form').on('click', () => {
-//         $('.show-comment-form').addClass('hidden')
-//         $('.comment-text-area').removeClass('hidden')
-//     })
-// }
-
-// const appendNewComment = (comment) => {
-//     $('.comments-container').append(
-//         `<div class="post_comment"><img src=${escape(comment.user.avatar_image)} /></div>`
-//         `<div class="post_comment"><p>${escape(comment.user.name)}</p></div>`
-//         `<div class="post_comment"><p>${escape(comment.content)}</p></div>`
-//     ) 
-// }
-
 document.addEventListener('turbolinks:load', () => {
 
     const dataset = $('#comment-index').data()
-    const postId = dataset.postId
+    if (dataset) {
+        const postId = dataset.postId
 
-    // axios.get(`/api/posts/${postId}/comments/new`)
-    // .then((response) => {
-    //     const comments = response.data
-    //     comments.forEach((comment) => {
-    //         appendNewComment(comment)
-    //     });
-    // })
-
-    // handleCommentForm()
-
-    const postComment = () => {
-        const content = $('#comment_content').val()
-        if (!content) {
-            window.alert('コメントを入力してください')
-        } else {
-            axios.post(`/api/posts/${postId}/comments`, {
-                comment: {content: content}
-            })
-                .then((res) => {
-                    $('#comment_content').val('')
-                    window.location.reload()
+        const postComment = () => {
+            const content = $('#comment_content').val()
+            if (!content) {
+                window.alert('コメントを入力してください')
+            } else {
+                axios.post(`/api/posts/${postId}/comments`, {
+                    comment: {content: content}
                 })
+                    .then((res) => {
+                        $('#comment_content').val('')
+                        window.location.reload()
+                    })
+            }
         }
+
+        // 画像をクリックしたときのイベント
+        $('.comment-icon').one('click', postComment)
     }
-
-    // 画像をクリックしたときのイベント
-    $('.comment-icon').on('click', postComment)
-
 })
