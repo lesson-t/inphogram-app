2c54735debdc174eade34987ea3ce0a801ba5083
takeshi <goozooloo123@gmail.com>
2024-10-16 08:20:25 +0900

    account.js 画面遷移によるエラー画面確認・修正, 不要なjsファイルの削除

--- app/javascript/packs/account.js
+++ app/javascript/packs/account.js
@@ -16,42 +16,44 @@ const handleFollowDisplay = (hasFollowed) => {
 document.addEventListener('turbolinks:load', () => {
 
     const dataset = $('#account-show').data()
-    const accountId = dataset.accountId
-    const followedId = dataset.followedId
+    if (dataset && dataset.accountId && dataset.followedId) {
+        const accountId = dataset.accountId
+        const followedId = dataset.followedId
 
-    axios.get(`/accounts/${accountId}/follows/${followedId}`)
-        .then((response) => {
-            const hasFollowed = response.data.hasFollowed
-            handleFollowDisplay(hasFollowed)
-    })
+        axios.get(`/accounts/${accountId}/follows/${followedId}`)
+            .then((response) => {
+                const hasFollowed = response.data.hasFollowed
+                handleFollowDisplay(hasFollowed)
+        })
 
-    // Followする時、Follow を押して Unfollow を表示
-    $('.active-unfollow').on('click', () => {
-        axios.post(`/accounts/${accountId}/follows`)
-        .then((response) => {
-            if (response.data.status == 'ok' ) {
-                $('.active-follow').removeClass('hidden')
-                $('.active-unfollow').addClass('hidden')
-            }
+        // Followする時、Follow を押して Unfollow を表示
+        $('.active-unfollow').on('click', () => {
+            axios.post(`/accounts/${accountId}/follows`)
+            .then((response) => {
+                if (response.data.status == 'ok' ) {
+                    $('.active-follow').removeClass('hidden')
+                    $('.active-unfollow').addClass('hidden')
+                }
+            })
+            .catch((e) => {
+                window.alert('Error')
+                console.log(e)
+            })
         })
-        .catch((e) => {
-            window.alert('Error')
-            console.log(e)
-        })
-    })
 
-    // Unfollowする時、Unfollow を押して Follow を表示
-    $('.active-follow').on('click', () => {
-        axios.post(`/accounts/${accountId}/unfollows`)
-        .then((response) => {
-            if (response.data.status == 'ok' ) {
-                $('.active-unfollow').removeClass('hidden')
-                $('.active-follow').addClass('hidden')
-            }
+        // Unfollowする時、Unfollow を押して Follow を表示
+        $('.active-follow').on('click', () => {
+            axios.post(`/accounts/${accountId}/unfollows`)
+            .then((response) => {
+                if (response.data.status == 'ok' ) {
+                    $('.active-unfollow').removeClass('hidden')
+                    $('.active-follow').addClass('hidden')
+                }
+            })
+            .catch((e) => {
+                window.alert('Error')
+                console.log(e)
+            })
         })
-        .catch((e) => {
-            window.alert('Error')
-            console.log(e)
-        })
-    })
+    }
 })
