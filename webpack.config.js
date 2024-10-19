bee5b9921992f66df15019559cf137c47f5caf54
takeshi <goozooloo123@gmail.com>
2024-10-19 17:35:42 +0900

    deploy-fix6 webpack系ver変更

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,18 @@ module.exports = {
       filename: 'bundle.js',
       path: path.resolve(__dirname, 'public/packs')
     },
-
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js'],
+    },
   };
