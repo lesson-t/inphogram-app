e7c6f57b786ddd693f1920536d3bdea8a198bbee
takeshi <goozooloo123@gmail.com>
2024-10-20 11:22:26 +0900

    deploy-fix12 babel関連修正

--- babel.config.js
+++ babel.config.js
@@ -41,34 +41,18 @@ module.exports = function(api) {
       '@babel/plugin-syntax-dynamic-import',
       isTestEnv && 'babel-plugin-dynamic-import-node',
       '@babel/plugin-transform-destructuring',
-      [
-        '@babel/plugin-proposal-class-properties',
-        {
-          loose: true
-        }
-      ],
-      ["@babel/plugin-transform-private-methods", { "loose": true }],
-      ["@babel/plugin-transform-private-property-in-object", { "loose": true }],
-      [
-        '@babel/plugin-proposal-object-rest-spread',
-        {
-          useBuiltIns: true
-        }
-      ],
-      [
-        '@babel/plugin-transform-runtime',
+      ["@babel/plugin-transform-class-properties", { loose: true }],
+      ["@babel/plugin-transform-private-methods", { loose: true }],
+      ["@babel/plugin-transform-private-property-in-object", { loose: true }],
+      ["@babel/plugin-transform-object-rest-spread", { useBuiltIns: true }],
+      ['@babel/plugin-transform-runtime',
         {
           helpers: false,
           regenerator: true,
           corejs: false
         }
       ],
-      [
-        '@babel/plugin-transform-regenerator',
-        {
-          async: false
-        }
-      ]
+      ['@babel/plugin-transform-regenerator', { async: false }]
     ].filter(Boolean)
   }
 }
